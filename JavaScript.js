4e4d7871f4c2997158776bb35c25bce20c093822
OliverLolk <Ollo14ac@student.cbs.dk>
2015-11-09 21:14:57 +0100

    Login...

--- JavaScript.js
+++ JavaScript.js
@@ -1,12 +1,13 @@
+
 /**
  * Created by oliverlolk on 09/11/2015.
  */
 $(document).ready(function(){
 
-      $('#loginbtn').click(function () {
+      $('#loginBtn').click(function() {
 
           alert("tobias");
-      });
+          });
 
 });
 

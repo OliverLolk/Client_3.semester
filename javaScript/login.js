17dc7dbba56881a80dfeb2f95f5b457ec83226e3
OliverLolk <Ollo14ac@student.cbs.dk>
2015-11-10 23:59:19 +0100

    Creating the different views for the client

--- javaScript/login.js
+++ javaScript/login.js
@@ -4,14 +4,29 @@
  */
 $(document).ready(function(){
 
-    var username
     $("#loginBtn").click(function(){
-        alert('Please fill out all the fields');
-        });
 
-    $("#registerBtn").click(function(){
-        alert('Remember to fill out anything in the formular');
-        });
+        var username = $("#Username").val();
+        var password = $("#Password").val();
+
+        if(username == "" && password == "") {
+            alert('Please remember to type both username and password');
+        }
+        else if(username == "Oliver" && password != "1234abcd") {
+            alert('Wrong password, try again');
+        }
+        else if(username != "Oliver" && password == "1234abcd") {
+            alert("something was wrong");
+        }
+        else if(username == "Oliver" && password == "1234abcd") {
+            window.location.href = "../HTML/userMenu.html";
+        }
+
+    });
+
+    //$("#registerBtn").click(function(){
+        //alert('Remember to fill out anything in the formular');
+        //});
 
 });
 
